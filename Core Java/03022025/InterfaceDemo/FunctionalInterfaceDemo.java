252a7864f9e4d8f9452dadecb4326a1ed9e1058f
shoyab.siddique@wissen.com <shoyab.siddique@wissen.com>
2025-02-27 10:42:04 +0530

    27-02-2025

--- Core Java/03022025/InterfaceDemo/FunctionalInterfaceDemo.java
+++ Core Java/03022025/InterfaceDemo/FunctionalInterfaceDemo.java
@@ -1,44 +1,52 @@
 package InterfaceDemo;
 
-@FunctionalInterface
-interface I {
-    public void abc();
-}
+// @FunctionalInterface
+// interface I {
+//     public void abc();
+// }
 
-class A implements I {
-    public void abc() {
-        System.out.println("this is from A");
-    }
-}
+// class A implements I {
+//     public void abc() {
+//         System.out.println("this is from A");
+//     }
+// }
 
-class B {
-    public void demo() {
-        System.out.println("this is from B");
-    }
-}
+// class B {
+//     public void demo() {
+//         System.out.println("this is from B");
+//     }
+// }
 
 public class FunctionalInterfaceDemo {
+    static int x = 10;
+
+    public int FunctionalInterfaceDemo() {
+        return 10;
+    }
+
     public static void main(String[] args) {
-        I i1 = new A();
-        i1.abc();
+        System.out.println(x + 5);
 
-        I i2 = new I() {
-            public void abc() {
-                System.out.println("Hello Everyone from anonomous class");
-            }
-        };
-        i2.abc();
-        B b1 = new B();
-        b1.demo();
+        // I i1 = new A();
+        // i1.abc();
 
-        B b2 = new B() {
-            public void demo() {
-                System.out.println("Changed functionality of class using anonomous class");
-            }
-        };
-        b2.demo();
+        // I i2 = new I() {
+        // public void abc() {
+        // System.out.println("Hello Everyone from anonomous class");
+        // }
+        // };
+        // i2.abc();
+        // B b1 = new B();
+        // b1.demo();
 
-        I i3 = () -> System.out.println("Hello from lambda expression");
-        i3.abc();
+        // B b2 = new B() {
+        // public void demo() {
+        // System.out.println("Changed functionality of class using anonomous class");
+        // }
+        // };
+        // b2.demo();
+
+        // I i3 = () -> System.out.println("Hello from lambda expression");
+        // i3.abc();
     }
 }
